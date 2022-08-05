919e61ffcf529bd40c075bd69702997f1ce329b2
rcampos27 <rodrigo@alkantara.com.br>
2022-08-04 23:38:28 -0300

    Commit 10

--- Movie.java
+++ Movie.java
@@ -44,4 +44,12 @@ public class Movie {
         return thisAmount;
     }
 
+    public int getFrequentRenterPoints(int daysRented) {// add frequent renter points
+        int frequentRenterPoints = 1;
+        // add bonus for a two day new release rental
+        if ((this._priceCode == Movie.NEW_RELEASE) &&
+                daysRented > 1) frequentRenterPoints++;
+        return frequentRenterPoints;
+    }
+
 }
--- Rental.java
+++ Rental.java
@@ -20,12 +20,7 @@ public class Rental {
         return _movie.getCharge(_daysRented);
     }
 
-    public int getFrequentRenterPoints() {// add frequent renter points
-        int frequentRenterPoints = 1;
-        // add bonus for a two day new release rental
-        if ((this.getMovie().getPriceCode() == Movie.NEW_RELEASE) &&
-                this.getDaysRented() > 1) frequentRenterPoints++;
-        return frequentRenterPoints;
+    public int getFrequentRenterPoints(){
+        return _movie.getFrequentRenterPoints(_daysRented);
     }
-
 }
