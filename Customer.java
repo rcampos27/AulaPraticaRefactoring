9bc0c61821ee8a95dc5674b1a69c548dad3b68dd
rcampos27 <rodrigo@alkantara.com.br>
2022-08-04 23:15:30 -0300

    Commit 4

--- Customer.java
+++ Customer.java
@@ -27,7 +27,7 @@ public class Customer {
             Rental each = (Rental) rentals.nextElement();
 
             //determine amounts for each line
-            thisAmount += amountFor(each);
+            thisAmount += each.getCharge();
 
             // add frequent renter points
             frequentRenterPoints ++;
@@ -48,23 +48,4 @@ public class Customer {
         return result;
     }
 
-    private double amountFor(Rental rental) {
-        double thisAmount = 0;
-        switch (rental.getMovie().getPriceCode()) {
-            case Movie.REGULAR:
-                thisAmount += 2;
-                if (rental.getDaysRented() > 2)
-                    thisAmount += (rental.getDaysRented() - 2) * 1.5;
-                break;
-            case Movie.NEW_RELEASE:
-                thisAmount += rental.getDaysRented() * 3;
-                break;
-            case Movie.CHILDRENS:
-                thisAmount += 1.5;
-                if (rental.getDaysRented() > 3)
-                    thisAmount += (rental.getDaysRented() - 3) * 1.5;
-                break;
-        }
-        return thisAmount;
-    }
 }
