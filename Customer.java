6df4f5a97c35772bac87966c0021b32ad7cfa067
rcampos27 <rodrigo@alkantara.com.br>
2022-08-04 23:19:45 -0300

    Commit 5

--- Customer.java
+++ Customer.java
@@ -16,18 +16,14 @@ public class Customer {
     public String getName (){
         return _name;
     }
-
+    
     public String statement() {
         double totalAmount = 0;
         int frequentRenterPoints = 0;
         Enumeration rentals = _rentals.elements();
         String result = "Rental Record for " + getName() + "\n";
         while (rentals.hasMoreElements()) {
-            double thisAmount = 0;
             Rental each = (Rental) rentals.nextElement();
-
-            //determine amounts for each line
-            thisAmount += each.getCharge();
 
             // add frequent renter points
             frequentRenterPoints ++;
@@ -35,16 +31,17 @@ public class Customer {
             if ((each.getMovie().getPriceCode() == Movie.NEW_RELEASE) &&
                     each.getDaysRented() > 1) frequentRenterPoints ++;
 
-            //show figures for this rental
-            result += "\t" + each.getMovie().getTitle()+ "\t" +
-                    String.valueOf(thisAmount) + "\n";
-            totalAmount += thisAmount;
+            // show figures for this rental
+            result += "\t" + each.getMovie().getTitle()+ "\t" + String.valueOf
+                    (each.getCharge()) + "\n";
+            totalAmount += each.getCharge();
 
         }
-        //add footer lines
+
+        // add footer lines
         result +=  "Amount owed is " + String.valueOf(totalAmount) + "\n";
-        result += "You earned " + String.valueOf(frequentRenterPoints) +
-                " frequent renter points";
+        result += "You earned " + String.valueOf(frequentRenterPoints)
+                + " frequent renter points";
         return result;
     }
 
